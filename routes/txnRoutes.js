34d48f11cbc403b9f185977f6059e8367858b67f
Willem Odendaal <willem@newmedialabs.co.za>
2015-07-22 12:59:42 +0200

    Moved router to its own file.

---
+++ routes/txnRoutes.js
@@ -0,0 +1,47 @@
+var express = require('express'),
+    Transaction = require('../models/transactionModel');
+
+
+var routes = function () {
+    var router = express.Router();
+    router.route('/transactions')
+        .post(function(req,res) {
+            var txn = new Transaction(req.body);
+
+            txn.save();
+            res.status(201).send(txn); //Status 201 = Created
+
+        })
+        .get(function (req, res) {
+
+            var query = {};
+
+            if (req.query.category) {
+                query.category = req.query.category;
+            }
+
+            Transaction.find(query, function (err, txns) {
+                if (err) {
+                    res.status(500).send(err);
+                }
+
+                res.json(txns);
+            });
+
+        });
+
+    router.route('/transactions/:id')
+        .get(function (req, res) {
+            Transaction.findById(req.params.id, function (err, txn) {
+                if (err)
+                    res.status(500).send(err);
+                else
+                    res.json(txn);
+            });
+        });
+
+    return router;
+
+};
+
+module.exports = routes;
--- server.js
+++ server.js
@@ -1,57 +1,17 @@
 var express = require('express'),
     mongoose = require('mongoose'),
     Transaction = require('./models/transactionModel'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    TxnRouter = require('./routes/txnRoutes');
 
 var db = mongoose.connect('mongodb://localhost/budget');
 var app = express();
 var port = process.env.PORT || 3001;
+
 app.use(bodyParser.json()); //Use middleware. Convert json in body to req.body.
 app.use(bodyParser.urlencoded({extended:true}));
-
-var router = express.Router();
-router.route('/transactions')
-    .post(function(req,res) {
-        var txn = new Transaction(req.body);
-
-        txn.save();
-        res.status(201).send(txn); //Status 201 = Created
-
-    })
-    .get(function (req, res) {
-
-        var query = {};
-
-        if (req.query.category) {
-            query.category = req.query.category;
-        }
-
-        Transaction.find(query, function (err, txns) {
-            if (err) {
-                res.status(500).send(err);
-            }
-
-            res.json(txns);
-        });
-
-    });
-
-router.route('/transactions/:id')
-    .get(function (req, res) {
-        Transaction.findById(req.params.id, function (err, txn) {
-            if (err)
-                res.status(500).send(err);
-            else
-                res.json(txn);
-        });
-    });
-
-app.use('/api', router);
-
-app.get('/', function (req, res) {
-    res.send('Aloha.');
-});
+app.use('/api', TxnRouter());
 
 app.listen(port, function () {
-    console.log('App listening on port ' + port);
+    console.log('API listening on port ' + port);
 });
