d10101f25ca821da3a6f6726d6e41c0e7022eedf
Willem Odendaal <willem@newmedialabs.co.za>
2015-08-22 10:29:35 +0200

    Flattened txnRoutes code structure.

--- routes/txnRoutes.js
+++ routes/txnRoutes.js
@@ -1,104 +1,113 @@
 var express = require('express'),
     Transaction = require('../models/transactionModel');
 
-var routes = function () {
+
+var txnRouter = function () {
     var router = express.Router();
     router.route('/')
-        .post(function (req, res) {
-            var txn = new Transaction(req.body);
+        .post(_postTxn)
+        .get(_findTxns);
 
-            txn.save();
-            res.status(201).send(txn); //Status 201 = Created
-
-        })
-        .get(function (req, res) {
-
-            var query = {};
-
-            if (req.query.category) {
-                query.category = req.query.category;
-            }
-
-            Transaction.find(query, function (err, txns) {
-                if (err) {
-                    res.status(500).send(err);
-                }
-
-                res.json(txns);
-            });
-
-        });
-
-    //Middleware to get Txn by ID, since we do this in more than one
-    //  route. We append Txn to the request.
-    router.use('/:id', function (req, res, next) {
-        Transaction.findById(req.params.id, function (err, txn) {
-            if (err)
-                res.status(500).send(err);
-            else if (txn) {
-                req.txn = txn;
-                next();
-            }
-            else {
-                res.status(404).send('No txn found.');
-            }
-        });
-    });
+    router.use('/:id', _injectTxnMiddleware);
 
     router.route('/:id')
-        .get(function (req, res) {
-            res.json(req.txn);
-        })
-        .put(function (req, res) {
-            //Find and update entire Transaction.
-            req.txn.title = req.body.title;
-            req.txn.description = req.body.description;
-            req.txn.category = req.body.category;
-            req.txn.amount = req.body.amount;
-            req.txn.read = req.body.read;
-
-            req.txn.save(function(err) {
-                if (err)
-                    res.status(500).send(err);
-                else {
-                    res.json(req.txn);
-                }
-            });
-        })
-        .patch(function (req, res) {
-
-            if (req.body._id) {
-                delete req.body._id; //We don't want to update this.
-            }
-
-            //Only update what is passed.
-            for(var p in req.body) {
-                req.txn[p] = req.body[p];
-            }
-
-            req.txn.save(function(err) {
-                if (err)
-                    res.status(500).send(err);
-                else {
-                    res.json(req.txn);
-                }
-            });
-
-        })
-        .delete(function(req,res) {
-
-            req.txn.remove(function(err) {
-                if (err)
-                    res.status(500).send(err);
-                else {
-                    //204 == No-content/Removed
-                    res.status(204).send('Txn deleted.');
-                }
-            });
-        });
+        .get(_getTxn)
+        .put(_putTxn)
+        .patch(_patchTxn)
+        .delete(_deleteTxn);
 
     return router;
-
 };
 
-module.exports = routes;
+function _findTxns(req, res) {
+    var query = {};
+
+    if (req.query.category) {
+        query.category = req.query.category;
+    }
+
+    Transaction.find(query, function (err, txns) {
+        if (err) {
+            res.status(500).send(err);
+        }
+
+        res.json(txns);
+    });
+}
+
+function _postTxn(req, res) {
+    var txn = new Transaction(req.body);
+
+    txn.save();
+    res.status(201).send(txn); //Status 201 = Created
+}
+
+function _getTxn(req, res) {
+    res.json(req.txn);
+}
+
+//Middleware to get Txn by ID, since we do this in more than one
+//  route. We append Txn to the request.
+function _injectTxnMiddleware(req, res, next) {
+    Transaction.findById(req.params.id, function (err, txn) {
+        if (err)
+            res.status(500).send(err);
+        else if (txn) {
+            req.txn = txn;
+            next();
+        }
+        else {
+            res.status(404).send('No txn found.');
+        }
+    });
+}
+
+function _putTxn(req, res) {
+    //Find and update entire Transaction.
+    req.txn.title = req.body.title;
+    req.txn.description = req.body.description;
+    req.txn.category = req.body.category;
+    req.txn.amount = req.body.amount;
+    req.txn.read = req.body.read;
+
+    req.txn.save(function(err) {
+        if (err)
+            res.status(500).send(err);
+        else {
+            res.json(req.txn);
+        }
+    });
+}
+
+function _patchTxn(req, res) {
+    if (req.body._id) {
+        delete req.body._id; //We don't want to update this.
+    }
+
+    //Only update what is passed.
+    for(var p in req.body) {
+        req.txn[p] = req.body[p];
+    }
+
+    req.txn.save(function(err) {
+        if (err)
+            res.status(500).send(err);
+        else {
+            res.json(req.txn);
+        }
+    });
+}
+
+function _deleteTxn(req,res) {
+
+    req.txn.remove(function(err) {
+        if (err)
+            res.status(500).send(err);
+        else {
+            //204 == No-content/Removed
+            res.status(204).send('Txn deleted.');
+        }
+    });
+}
+
+module.exports = txnRouter;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express'),
-    mongoose = require('mongoose'),
+    mongoose = require('mongoose'), //to access MongoDb
     Transaction = require('./models/transactionModel'),
     bodyParser = require('body-parser'),
     TxnRouter = require('./routes/txnRoutes');
@@ -8,10 +8,18 @@ var db = mongoose.connect('mongodb://localhost/budget');
 var app = express();
 var port = process.env.PORT || 3001;
 
-app.use(bodyParser.json()); //Use middleware. Convert json in body to req.body.
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json()); //Middleware that decodes json bodies.
+app.use(bodyParser.urlencoded({extended:true})); //Middleware that decodes form-encoded bodies.
 app.use('/api/transactions', TxnRouter());
 
 app.listen(port, function () {
     console.log('API listening on port ' + port);
+});
+
+//Close mongoose connection when app stops.
+process.on('SIGINT', function() {
+    mongoose.connection.close(function() {
+        console.log('Closed mongoose connection.');
+        process.exit(0);
+    });
 });
