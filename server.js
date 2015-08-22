4ef1a1f8b2893d7627deb4f0b82e0abae9449121
Willem Odendaal <willem@newmedialabs.co.za>
2015-08-22 10:52:31 +0200

    Moved javascript to 'src' folder and added some documentation.

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express'),
     mongoose = require('mongoose'), //to access MongoDb
-    Transaction = require('./models/transactionModel'),
     bodyParser = require('body-parser'),
-    TxnRouter = require('./routes/txnRoutes');
+    Transaction = require('./src/models/transactionModel'),
+    TxnRouter = require('./src/routes/txnRoutes');
 
 var db = mongoose.connect('mongodb://localhost/budget');
 var app = express();
@@ -13,10 +13,10 @@ app.use(bodyParser.urlencoded({extended:true})); //Middleware that decodes form-
 app.use('/api/transactions', TxnRouter());
 
 app.listen(port, function () {
-    console.log('API listening on port ' + port);
+    console.log('BudgetAPI listening on port ' + port);
 });
 
-//Close mongoose connection when app stops.
+//Clean-up properly. Close mongoose connection when app stops.
 process.on('SIGINT', function() {
     mongoose.connection.close(function() {
         console.log('Closed mongoose connection.');
--- src/models/transactionModel.js
+++ src/models/transactionModel.js
@@ -1,3 +1,10 @@
+/**
+ *  Provides database access to Transaction objects. Exports Transaction model.
+ *
+ * @module transactionModel
+ * @type {Transaction}
+ */
+
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
@@ -9,6 +16,13 @@ var transactionSchema = new Schema({
     read: { type: Boolean, default: false }
 });
 
+/**
+ * Map all fields from a Transaction onto this Transaction, even
+ *  null/empty fields.
+ *
+ * @method mapAllFieldsFrom
+ * @param txn {Transaction} The transaction to map from onto this instance.
+ */
 transactionSchema.methods.mapAllFieldsFrom = function(txn) {
     this.title = txn.title;
     this.description = txn.description;
@@ -17,6 +31,12 @@ transactionSchema.methods.mapAllFieldsFrom = function(txn) {
     this.read = txn.read;
 };
 
+/**
+ * Map only the fields that were passed from a Transaction onto this transaction.
+ * The _id property will be ignored if passed.
+ *
+ * @param txn {Transaction} The transaction to map from onto this instance.
+ */
 transactionSchema.methods.mapPassedFieldsFrom = function(txn) {
     if (txn._id) {
         delete txn._id; //We don't want to update the id.
@@ -27,6 +47,5 @@ transactionSchema.methods.mapPassedFieldsFrom = function(txn) {
         this[p] = txn[p];
     }
 };
-
 
 module.exports = mongoose.model('Transaction', transactionSchema);
