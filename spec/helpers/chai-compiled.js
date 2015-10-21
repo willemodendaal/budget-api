b6135dff5708237aa9e18a8c931028e22c40fc29
Willem Odendaal <willem@newmedialabs.co.za>
2015-10-21 18:00:43 +0200

    Implemented basic db setup/teardown with mongoose. Must still implement actual REST tests.

---
+++ spec/helpers/chai-compiled.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var chai = require('chai');
+chai.config.includeStack = true;
+
+global.sinon = require('sinon');
+global.expect = chai.expect;
+global.AssertionError = chai.AssertionError;
+global.Assertion = chai.Assertion;
+
+//# sourceMappingURL=chai-compiled.js.map
--- spec/test_spec.js
+++ spec/test_spec.js
@@ -1,35 +1,55 @@
 var mongoose = require('mongoose'),
     Budget = require('../src/models/budgetModel');
 
+mongoose.disconnect();
+mongoose.connect('mongodb://localhost/budget_test');
 
 describe("Budget repository", function () {
-    beforeAll(function (done) {
-        this.db = mongoose.connect('mongodb://localhost/budget_test');
+    before(function (done) {
+        console.log('Seeding db...');
         _seedDb(done);
-        console.log('Seeded db...');
+    });
+
+    after(function(done) {
+        //delete budgets
+        console.log('Cleaning db...');
+        Budget.find().remove(function(err) {
+            if (err) {
+                throw 'error clening db:' + err;
+            }
+
+            done();
+        });
+
     });
 
     it("has one budget.", function (done) {
         Budget.find(function (err, budgets) {
             if (err) {
-                fail(err);
+                throw 'error finding budget:' + err;
             }
 
-            expect(budgets).toBe(jasmine.anything());
+            expect(budgets).to.exist;
+            expect(budgets).to.not.be.empty;
+            expect(budgets.length).to.equal(1);
             done();
         });
     });
 
     var _seedDb = function (done) {
         var budget = new Budget({
-            title: 'test b1'
+            title: 'test b1',
+            dateFrom: '19 may 2005',
+            dateTo: '19 may 2005'
         });
 
         //Create a budget.
         budget.save(function (err, budget) {
             if (err) {
-                fail(err);
+                throw 'Error saving budget: ' + err;
             }
+
+            console.log('Seeded db.');
             done();
         });
     };
