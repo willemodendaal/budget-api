39107946a1568403c39d8b77979266422dcfd10c
willem <willem.odendaal@gmail.com>
2016-03-18 16:59:57 +0200

    Changed helper methods to promises. Easier to chain.

--- spec/testBudgetCrud.js
+++ spec/testBudgetCrud.js
@@ -19,11 +19,8 @@ describe("Budget api crud methods", function () {
         var dateFrom = new Date();
         var dateTo = new Date();
 
-        _createBudget(
-            'create budget test',
-            dateFrom,
-            dateTo,
-            function (newBudget) {
+        _createBudget('create budget test', dateFrom, dateTo)
+	        .then(function (newBudget) {
                 expect(newBudget.title).to.equal('create budget test');
                 //expect(newBudget.dateFrom).to.equal(new Date(dateFrom));
                 //expect(newBudget.dateTo).to.equal(new Date(dateTo));
@@ -34,46 +31,46 @@ describe("Budget api crud methods", function () {
     });
 
     it('updates(puts) a budget', function(done) {
-        _createBudget('toPut', new Date(), new Date(), function(budget) {
-	        var id = budget._id;
-	        budget.title = 'toPutUpdated';
+        _createBudget('toPut', new Date(), new Date())
+	        .then(function(budget) {
+		        var id = budget._id;
+		        budget.title = 'toPutUpdated';
 
-	        _putBudget(id, budget, function(updated) {
-		        expect(updated.title).to.equal('toPutUpdated');
-		        done();
-	        });
-        });
+		        _putBudget(id, budget, function(updated) {
+			        expect(updated.title).to.equal('toPutUpdated');
+			        done();
+		        });
+            });
     });
 
 	it('updates(patches) a budget', function(done) {
-		_createBudget('toPatch', new Date(), new Date(), function(budget) {
-			var id = budget._id;
-			budget.description = 'patched';
+		_createBudget('toPatch', new Date(), new Date())
+			.then(function(budget) {
+				var id = budget._id;
+				budget.description = 'patched';
 
-			_patchBudget(id, budget, function(updated) {
-				expect(updated.title).to.equal('toPatch');
-				expect(updated.description).to.equal('patched');
-				done();
+				_patchBudget(id, budget, function(updated) {
+					expect(updated.title).to.equal('toPatch');
+					expect(updated.description).to.equal('patched');
+					done();
+				});
 			});
-		});
 	});
 
     it('deletes a budget', function(done) {
 
-	    new Promise(function(resolve, reject) {
-		        _createBudget('toDelete', new Date(), new Date(), (b) => resolve(b));
-	        })
+	    _createBudget('toDelete', new Date(), new Date())
 		    .then(function(budget) {
 			    //GET the budget, ensure it exists.
-			    return new Promise((resolve, reject) => _getBudget(budget._id, (b)=> resolve(b)) );
+			    return _getBudget(budget._id);
 		    })
 		    .then(function(budget) {
 			    //Delete the budget.
-			    return new Promise((resolve, reject) => _deleteBudget(budget._id, ()=> resolve(budget._id)) );
+			    return _deleteBudget(budget._id);
 		    })
 		    .then(function(budgetId) {
 			    //GET the budget (expected to be null).
-			    return new Promise((resolve, reject) => _getBudget(budgetId, (nullBudget)=> resolve(nullBudget)) );
+			    return _getBudget(budgetId);
 		    })
 		    .then(function(nullBudget) {
 			    expect(nullBudget).to.be.null;
@@ -83,50 +80,31 @@ describe("Budget api crud methods", function () {
 			    console.log(err);
 			    throw err;
 		    });
-
-
-/*
-	    _createBudget('toDelete', new Date(), new Date(), function(budget) {
-		    var originalId = budget._id;
-
-		    //GET the budget, ensure it exists.
-			_getBudget(originalId, function(fetched1) {
-				expect(fetched1._id).to.equal(originalId);
-
-				//Delete the budget.
-				_deleteBudget(originalId, function() {
-
-					//Try to GET again and expect a 404.
-					_getBudget(originalId, function(fetched2) {
-						expect(fetched2).to.be.null;
-						//done();
-					});
-				});
-			});
-	    });
-*/
     });
 
-    function _createBudget(name, dateFrom, dateTo, callback) {
-        request.post({
-                url: Constants.urls.budgetsUrl(),
-                json: true,
-                body: {
-                    title: name,
-                    dateFrom: dateFrom,
-                    dateTo: dateTo
-                }
-            },
-            function (err, res, body) {
-	            if (err) {
-		            console.log('Error creating: ', err, res, body);
-	            }
-                expect(err).to.be.null;
-                expect(res.statusCode).to.equal(201); //201 == created
-                callback(body);
-            }
-        );
-    }
+	function _createBudget(name, dateFrom, dateTo) {
+		return new Promise((resolve, reject) => {
+			request.post({
+					url: Constants.urls.budgetsUrl(),
+					json: true,
+					body: {
+						title: name,
+						dateFrom: dateFrom,
+						dateTo: dateTo
+					}
+				},
+				function (err, result, body) {
+					if (err) {
+						console.log('Error creating: ', err, result, body);
+						reject(err);
+					}
+					expect(err).to.be.null;
+					expect(result.statusCode).to.equal(201); //201 == created
+					resolve(body);
+				}
+			);
+		});
+	}
 
 	function _putBudget(id, budget, callback) {
 		request.put({
@@ -146,42 +124,46 @@ describe("Budget api crud methods", function () {
 		);
 	}
 
-	function _getBudget(id, callback) {
-		request.get({
-				url: Constants.urls.budgetUrl(id),
-				json: true
-			},
-			function (err, res, body) {
-				if (err) {
-					console.log('Error getting budget: ', err, res, body);
-				}
+	function _getBudget(id) {
+		return new Promise((resolve, reject) =>
+			request.get({
+					url: Constants.urls.budgetUrl(id),
+					json: true
+				},
+				function (err, res, body) {
+					if (err) {
+						console.log('Error getting budget: ', err, res, body);
+						reject(err);
+					}
 
-				if (res.statusCode === 404) {
-					callback(null);
+					if (res.statusCode === 404) {
+						resolve(null);
+					}
+					else {
+						expect(err).to.be.null;
+						expect(res.statusCode).to.equal(200);
+						resolve(body);
+					}
 				}
-				else {
-					expect(err).to.be.null;
-					expect(res.statusCode).to.equal(200);
-					callback(body);
-				}
-			}
-		);
+		));
 	}
 
-	function _deleteBudget(id, callback) {
-		request.del({
-				url: Constants.urls.budgetUrl(id)
-			},
-			function (err, res, body) {
-				if (err) {
-					console.log('Error deleting budget: ', err, res, body);
-				}
+	function _deleteBudget(id) {
+		return new Promise((resolve, reject) =>
+			request.del({
+					url: Constants.urls.budgetUrl(id)
+				},
+				function (err, res, body) {
+					if (err) {
+						console.log('Error deleting budget: ', err, res, body);
+						reject(err);
+					}
 
-				expect(err).to.be.null;
-				expect(res.statusCode).to.equal(204); //204 = Deleted
-				callback();
-			}
-		);
+					expect(err).to.be.null;
+					expect(res.statusCode).to.equal(204); //204 = Deleted
+					resolve(id);
+				}
+			));
 	}
 
 	function _patchBudget(id, budget, callback) {
